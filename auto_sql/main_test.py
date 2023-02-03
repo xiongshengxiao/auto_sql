dcf8044ce21268209c631e2ec791f3044f38c497
YIXIAO <59684902+xiongshengxiao@users.noreply.github.com>
2023-02-03 09:19:58 +0800

    Update main_test.py

--- auto_sql/main_test.py
+++ auto_sql/main_test.py
@@ -13,12 +13,12 @@ from func import test_functions as func
 
 if __name__ == "__main__":
     pro = {
-        # "old_dl_excel_path": r'C:\Users\BLSJ\Desktop\【销售订单】新DMS入湖变更及整合DWD要件v1.6.xlsx',
+        "old_dl_excel_path": r"E:\GuangFeng\【线索】新DMS入湖变更及整合-DL方案要件V2.4.xlsx",
         "ods_excel_path": r"E:\GuangFeng\dms二手车表结构_自己.xlsx",
         "dwd_excel_path": r'D:\DMS\02要件设计\各模块要件中间版本【持续添加】\5.1-DWD\要件\售后\售后保修\【保修】新DMS入湖变更及整合DWD方案要件v2.0.xlsx',
         "target_old_dl_test_sqls": "old_dl/old_dl_test_sqls.sql",
         "target_dwd_test_sqls": "dwd/dwd_test_sqls.sql",
-        "test_flag": 'ods',
+        "test_flag": 'dl',
         # ods表名-业务主键字典：代码会根据这个字典读取对应表名做ods测试
         "ods_table_list":{}
 
